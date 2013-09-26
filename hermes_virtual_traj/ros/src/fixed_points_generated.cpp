376d8f762fd41043f0f3a9efdf21e70dbb0fa316
rmorales <rmorales@umh.es>
2013-09-26 22:28:41 +0200

    kdl path ready falta introducir ros moveit

--- hermes_virtual_traj/ros/src/fixed_points_generated.cpp
+++ hermes_virtual_traj/ros/src/fixed_points_generated.cpp
@@ -15,16 +15,25 @@
 #include <kdl/rotational_interpolation_sa.hpp>
 #include <kdl/utilities/error.h>
 #include <kdl/trajectory_composite.hpp>
+#include <moveit/move_group_interface/move_group.h>
 
 
 int main(int argc,char* argv[]) {
+	ros::init(argc, argv, "move_group_interface_demo", ros::init_options::AnonymousName);
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
 
 	try{
 			// Creación de Frame Inicial (de q_rightIni matlab)
 			KDL::Frame initFrame(KDL::Rotation::Quaternion(0.0243,0.7940,-0.0578,-0.647),KDL::Vector(0.5277,-0.1258,-0.8949));
 			KDL::Frame targetFrame(KDL::Rotation::Quaternion(0.8971,0.2642,-0.3284,-0.1324),KDL::Vector(0.7342,0.1730,-0.1488));
+			KDL::Frame tmpFrame;
 
 			KDL::Path_Line* path = new KDL::Path_Line(initFrame,targetFrame,new KDL::RotationalInterpolation_SingleAxis(),1.0,true);
+
+			// Confirmado tenemos el path perfectamente definido para pasar lo a ROS moveit
+
+
 
 
 	}
